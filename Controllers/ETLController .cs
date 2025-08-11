8302240efe1631ad2d5183e309fb227c65bd1301
argom <argom@ARGOM>
2025-08-11 16:17:57 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/ETLController .cs
@@ -0,0 +1,52 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+using WebApplication1.Utils;
+
+public class ETLController : Controller
+{
+    private readonly ETLService _etlService;
+
+    public ETLController()
+    {
+        // Poné tu cadena de conexión acá
+        string connectionString = "Server=ARGOM\\SQLEXPRESS;Database=ETL;Trusted_Connection=True;";
+        _etlService = new ETLService(connectionString);
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> UploadExcel()
+    {
+        var file = Request.Form.Files[0];
+        if (file == null || file.Length == 0)
+        {
+            ViewBag.Message = "No se seleccionó archivo.";
+            return View("Index");
+        }
+
+        var filePath = Path.GetTempFileName();
+
+        using (var stream = new FileStream(filePath, FileMode.Create))
+        {
+            await file.CopyToAsync(stream);
+        }
+
+        List<Persona> personas = _etlService.LeerExcel(filePath);
+        _etlService.GuardarEnBase(personas);
+
+        ViewBag.Message = $"{personas.Count} registros cargados correctamente.";
+       // return View("Index");
+        return View("~/Views/Home/Index.cshtml");
+
+    }
+}
---
+++ Utils/ETLService.cs
@@ -0,0 +1,72 @@
+﻿namespace WebApplication1.Utils
+{
+    using System.Collections.Generic;
+    using System.Data.SqlClient;
+    using System.IO;
+    using OfficeOpenXml;
+    using WebApplication1.Models;
+    using System.Data.SqlClient;
+    using OfficeOpenXml;
+    public class ETLService
+    {
+        private readonly string _connectionString;
+
+        public ETLService(string connectionString)
+        {
+            _connectionString = connectionString;
+            //ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            ExcelPackage.License.SetNonCommercialPersonal("Tu Nombre");
+
+
+
+            // necesario para EPPlus
+        }
+
+        public List<Persona> LeerExcel(string filePath)
+        {
+            var personas = new List<Persona>();
+
+            using (var package = new ExcelPackage(new FileInfo(filePath)))
+            {
+                var worksheet = package.Workbook.Worksheets[0];
+                int rowCount = worksheet.Dimension.Rows;
+
+                for (int row = 2; row <= rowCount; row++) // fila 1 son headers
+                {
+                    var nombre = worksheet.Cells[row, 1].Text;
+                    var edadText = worksheet.Cells[row, 2].Text;
+                    int edad = 0;
+                    int.TryParse(edadText, out edad);
+
+                    personas.Add(new Persona
+                    {
+                        Nombre = nombre,
+                        Edad = edad
+                    });
+                }
+            }
+
+            return personas;
+        }
+
+        public void GuardarEnBase(List<Persona> personas)
+        {
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+                foreach (var persona in personas)
+                {
+                    var query = "INSERT INTO Personas (Nombre, Edad) VALUES (@Nombre, @Edad)";
+                    using (var cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@Nombre", persona.Nombre);
+                        cmd.Parameters.AddWithValue("@Edad", persona.Edad);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+    }
+
+}
